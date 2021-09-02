3129846feab055238b9946f125d406685f07776c
midelrocks <72671950+midelrocks@users.noreply.github.com>
2021-09-02 12:47:24 +0200

    Added Fonts, Added Gnome-KDE-Cinnamon & Corrected Reflector Parameters

--- xfce.sh
+++ xfce.sh
@@ -21,13 +21,31 @@ makepkg -si --noconfirm
 #pikaur -S --noconfirm auto-cpufreq
 #sudo systemctl enable --now auto-cpufreq
 
+# XFCE4
 sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies firefox arc-gtk-theme arc-icon-theme obs-studio vlc
+# GNOME
+#sudo pacman -S --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc
+# KDE
+#sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox simplescreenrecorder obs-studio vlc papirus-icon-theme kdenlive materia-kde
+# CINNAMON
+#sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon firefox simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc xed xreader metacity gnome-shell
+
+
+# FONTS
 sudo pacman -S --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-sherif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 sudo flatpak install -y spotify
 sudo flatpak install -y kdenlive
 
+# XFCE4 
 sudo systemctl enable lightdm
+# GNOME
+#sudo systemctl enable gdm
+# KDE
+#sudo systemctl enable sddm
+# CINNAMON
+#sudo systemctl enable lightdm
+
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 sudo reboot
